6bb0a5953cbb643a1da52dafbb688e232661828a
codeWithNithin <nithinkottary25@gmail.com>
2025-06-23 17:33:42 +0530

    Day-14 intro to js almost completed

---
+++ Front-End/14- introduction-to-js/coding/script.js
@@ -0,0 +1,51 @@
+//1. console message
+console.log("Hello Cohort");
+
+// 2. console methods
+
+console.log("Hello");
+console.err("typeErr");
+console.warn("dep warning");
+
+// 3. different types of values
+
+console.log('nithin')
+console.log(23)
+console.log(true)
+console.log(undefined)
+console.log(null)
+
+// variables
+// declaration
+let age;
+
+// initialization
+let name = "Virat";
+
+// reinitialization
+name = "kohli";
+
+// How to use variables
+console.log(name);
+console.log(age);
+
+// Arithmatic operations
+let num1 = 10;
+let num2 = 10;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+
+// alert(), confirm(), prompt()
+
+alert('Are u 18?')
+
+// confirm will have 2 buttons yes and no, on click of yes it will return true else it will listen false.
+confirm('Are you a man?')
+
+// to take input from user, we can use prompt.
+prompt('Enter your name:')
